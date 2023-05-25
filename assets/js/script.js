79fb8e69c9521ea925697dbc89018e857337c502
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-05-25 18:00:42 -0500

    cleaned up JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,12 @@ var currentDate = dayjs().format('dddd, MMMM D, YYYY'); // current date in forma
 $(document).ready(function () {
 
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-
   $('.saveBtn').on('click', function (event) {
     event.preventDefault();
     var time = $(this).parent().attr('id');
     var text = $(this).siblings('.description').val();
     localStorage.setItem(time, text);
-});
+  });
 
 
   //Checks current time and then applies styling to each time block based on logic
@@ -33,35 +25,28 @@ $(document).ready(function () {
         hour.addClass('past');
       } else {
         hour.addClass('future');
+      }
+
     }
-     
-  }
-    
+
   }
 
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-  //displays current day and time at top of page
-  $('#currentDay').text(currentDate);
-  
-  //executes checkTime function upon page load, and then every 5 minutes page is open
-  checkTime();
-  setInterval(checkTime, 300000);
-  getSavedItems();
-
+  //checks localStorage and displays any saved values to their respective blocks 
   function getSavedItems() {
     for (i = 9; i < 18; i++) {
       var key = '0' + i;
       var info = $('.description');
       $('#0' + i).children(info).val(localStorage.getItem(key));
-
     }
-
-    
   }
+
+  //displays current day and time at top of page
+  $('#currentDay').text(currentDate);
+
+  //executes checkTime function upon page load, and then every 5 minutes page is open, and gets saved items from localStorage
+  checkTime();
+  setInterval(checkTime, 300000);
+  getSavedItems();
+
 });
 
